dccb417f3804adbc13337e785d90951f8c9742a4
Frank Ralf <webmaster@nakohdo.de>
2011-06-29 20:59:35 +0200

    improved regex: deletes onclick attribute

--- filter.php
+++ filter.php
@@ -4,49 +4,50 @@
  * The idea is from Timothy Takemoto, see "Automatic pronounciatin guide"
  * http://moodle.org/mod/forum/discuss.php?d=162387
  *
- * Moodle glossary link pattern:
+ * Moodle 1.9 glossary link pattern:
  * /moodle/mod/glossary/showentry.php?courseid=1&concept=gorilla
  *
- * Replacement pattern and default URL: http://www.forvo.com/word/{searchterm}#en
+ * Replacement pattern and default URL: http://en.wikipedia.org/wiki/{glossaryterm}
  */
 
 function autolinkhijacker_filter($courseid, $text) {
 
     global $CFG;
+
     // Derive replacement URL from replacement pattern from settings.
     if (!isset($CFG->filter_autolinkhijacker_url)){
         set_config('filter_autolinkhijacker_url',
             get_string('urldefault', 'filter_autolinkhijacker')
         );
     }
-    
-    $replacement_pattern = $CFG->filter_autolinkhijacker_url;
 
-    preg_match_all (
+    $replacementpattern = $CFG->filter_autolinkhijacker_url;
+
+    preg_match(
         '/(.+)\{glossaryterm\}(.*)/i',
-        $replacement_pattern,
-        $matches
+        $replacementpattern,
+        $urlparts
     );
 
-    $url_start  = $matches[1][0];
-    $url_end    = $matches[2][0];
+    $urlstart   = $urlparts[1];
+    $urlend    = $urlparts[2];
 
-    // Replace the target URL of all glossary auto-links.
-    
+    // Replace the target URL of all glossary auto-links. Capture original parts for possible re-use.
     $regex = '/
         <a
         .+?
-        class="(?P<class>[^"]*)"
+        class="([^"]*)"                  # $1
         .+?
-        title="([^"]*)"
+        title="([^"]*)"                     # $2
         .+?
-        href="(.+?concept=(.+?))"
+        href="(.+?concept=(.+?))"  # $3 (href) $4 (concept)
+        .+?
         >
         /six';
-    
-    $text = preg_replace (
+
+    $text = preg_replace(
         $regex,
-        "<a href=\"$url_start$4$url_end\" target='_blank' title='$2'>",
+        "<a href=\"$urlstart$4$urlend\" target='_blank' title='$2' class='autolinkhijacker' style='outline: 2px dashed lime; background:yellow;'>",
         $text
     );
     return $text;
