6d40c937879a444be456d57c6b6572e6aca2850f
Frank Ralf <webmaster@nakohdo.de>
2011-07-22 22:13:20 +0200

    larger input field for URL (50)

--- filtersettings.php
+++ filtersettings.php
@@ -1,10 +1,13 @@
 <?php
 /*
- * This is the filter settings file for the Auto-link Hijacker filter for Moodle 2.0.
+ * This is the filter settings file for the Auto-dlink Hijacker filter for Moodle 2.0.
  * You can enter a replacement URL for glossary autolinking.
  */
 
 $settings->add(new admin_setting_configtext('filter_autolinkhijacker_url',
     get_string('url', 'filter_autolinkhijacker'),
     get_string('urlconfig', 'filter_autolinkhijacker'),
-    get_string('urldefault', 'filter_autolinkhijacker')));
+    get_string('urldefault', 'filter_autolinkhijacker'),
+    'PARAM_RAW',
+    50
+    ));
