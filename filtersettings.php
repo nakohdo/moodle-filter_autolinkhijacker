c3b74bc0801f875948c15a4237b8cd6a3e9d79ee
Frank Ralf <webmaster@nakohdo.de>
2011-07-23 16:06:02 +0200

    parameter type PARAM_RAW as constant instead of string

--- filtersettings.php
+++ filtersettings.php
@@ -4,10 +4,11 @@
  * You can enter a replacement URL for glossary autolinking.
  */
 
-$settings->add(new admin_setting_configtext('filter_autolinkhijacker_url',
+$settings->add(new admin_setting_configtext(
+    'filter_autolinkhijacker_url',
     get_string('url', 'filter_autolinkhijacker'),
     get_string('urlconfig', 'filter_autolinkhijacker'),
     get_string('urldefault', 'filter_autolinkhijacker'),
-    'PARAM_RAW',
+    PARAM_RAW,
     50
     ));
