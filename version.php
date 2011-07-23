78623e617621f66c0c5de4500680544d58025c9c
Frank Ralf <webmaster@nakohdo.de>
2011-07-23 16:32:23 +0200

    version file added

---
+++ version.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Auto-link Hijacker filter
+ *
+ * @package    filter
+ * @copyright  2011 Frank Ralf {@link webmaster@nakohdo.de}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+ $plugin->version = 2011072301;
